f0029df3669bf91d02fed481624257dafd7871bb
HENRIQUE SASAKI TANNOUS <henrique_tannous@estudante.sesisenai.org.br>
2026-06-26 11:32:56 -0300

    first commit

--- src/server.js
+++ src/server.js
@@ -1,1 +1,27 @@
 import express from 'express'
+import {prisma} from "./lib/prisma.ts"
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+app.post('/produtos', async (req, res) => {
+    const {nome, categoria, quantidade} = req.body;
+    try {
+        const novoProduto = await prisma.produto.create({
+            data: {
+                nome,
+                categoria,
+                quantidade: parseInt(quantidade, 10)
+            }
+        })
+        return res.status(201).json(novoProduto);
+    } catch (error) {
+    return res.status(500).json({error: "Erro ao cadastrar produto"});
+    }
+})
+
+app.listen(port, () => {console.log("Servidor rodando");
+})
